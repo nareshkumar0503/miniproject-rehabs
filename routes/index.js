03a2fb971eaab0ab0575ec26adfc6922f418e1d9
NishanthThangasamy <nishanthrko2000@gmail.com>
2024-09-29 23:13:17 +0530

    cleaned unwanted

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,6 @@ router.get('/descPage', indexCtrl.getDescPage);
 router.post('/book-appointment', indexCtrl.postAppointment);
 router.get('/view-appointment', indexCtrl.getAppointment);
 router.post('/cancel-appointment', indexCtrl.cancelAppointment);
-
-
 // ---------------------------------------------------------------------------------------------------------
 
 // *****Center *****Center *****Center *****Center *****Center *****Center *****Center ***** */
@@ -20,45 +18,6 @@ router.post('/add-events', indexCtrl.postEvent);
 router.get('/center-view-appointment/:email', indexCtrl.getAppointments);
 
 // ------------------------------------------------------------------------------------------------------------
-
-
-// ---------------------------------------------------------------------------------------------------------
-
-
-
-// Approve appointment logic
-// router.post('/appointments/approve/:id', async (req, res) => {
-//   try {
-//       const appointmentId = req.params.id;
-//       const time = req.body.time;
-//       const timeString = String(time);
-//       // Logic to approve the appointment, e.g., updating the status
-//       await Appointment.findByIdAndUpdate(appointmentId, { 
-//         status: 'Approved',
-//         time : timeString });
-//         res.status(200); // Redirect back to appointments page
-//   } catch (error) {
-//       console.error('Error approving appointment:', error);
-//       res.status(500).send('Internal Server Error');
-//   }
-// });
-// -------------------------------------------------------------------------------------------------------------------------
-
-// router.post('/appointments/cancel/:id', async (req, res) => {
-//   try {
-//     const appointmentId = req.params.id;
-//     const time = req.body;
-//     // Logic to approve the appointment, e.g., updating the status
-//     await Appointment.findByIdAndUpdate(appointmentId, { 
-//       status: 'Appointment Canceled'
-//       });
-//       res.status(200); // Redirect back to appointments page
-//     } catch (error) {
-//         console.error('Error approving appointment:', error);
-//         res.status(500).send('Internal Server Error');
-//     } 
-// });
-// -------------------------------------------------------------------------------------------------------------------------
 
 //events
 router.get('/events-my-events', async(req,res)=>{
